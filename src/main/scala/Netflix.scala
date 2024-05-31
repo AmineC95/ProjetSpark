b5b4e523efbaf733f6f89dd02b0c7f54ccb01847
AmineC95 <amine.chioukh95@gmail.com>
2024-05-31 12:51:29 +0200

    neftlix

--- src/main/scala/Netflix.scala
+++ src/main/scala/Netflix.scala
@@ -3,8 +3,53 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 object Netflix {
-    var pathRoute ="../../../inputs/netflix_titles.csv"
+    var pathRoute = "inputs/netflix_titles.csv"
     val spark = SparkSession.builder.getOrCreate()
 
-    val df = spark.read.option("header", true).option("multiLine", true).option("escape", "\"").csv(pathRoute.split(";").mkString(","))
+    val df = spark.read.option("header", true).option("inferSchema", "true").option("multiLine", true).option("escape", "\"").csv(pathRoute)
+
+    val cleanedDf = df.na.drop(Seq("director", "cast", "country", "date_added", "release_year", "duration"))
+
+    // Lister les réalisateurs les plus prolifiques, et leur nombre de films respectifs
+    val prolificDirectors = cleanedDf.filter(col("director").isNotNull)
+        .groupBy("director")
+        .count()
+        .orderBy(desc("count"))
+    prolificDirectors.show()
+
+    // Afficher en pourcentages les pays dans lesquels les films/séries ont été produits
+    val countryCounts = cleanedDf.filter(col("country").isNotNull)
+        .groupBy("country")
+        .count()
+    val totalProductions = countryCounts.agg(sum("count")).first().getLong(0)
+    val countryPercentages = countryCounts.withColumn("percentage", (col("count") / totalProductions) * 100)
+        .orderBy(desc("count"))
+    countryPercentages.show()
+
+    // Quelle est la durée moyenne des films sur Netflix? Le film le plus long? Le film le plus court?
+    val movies = cleanedDf.filter(col("type") === "Movie")
+    val durations = movies.withColumn("duration", regexp_extract(col("duration"), "\\d+", 0).cast(IntegerType))
+    val avgDuration = durations.agg(avg("duration")).first().getDouble(0)
+    val longestMovie = durations.orderBy(desc("duration")).filter(col("duration").isNotNull).select("title", "duration").first()
+    val shortestMovie = durations.orderBy(asc("duration")).filter(col("duration").isNotNull).select("title", "duration").first()
+
+    println(s"La duree moyenne des films sur Netflix est de ${avgDuration} minutes.")
+    println(s"Le film le plus long est '${longestMovie.getString(0)}' avec ${longestMovie.getInt(1)} minutes.")
+    println(s"Le film le plus court est '${shortestMovie.getString(0)}' avec ${shortestMovie.getInt(1)} minutes.")
+
+    // Afficher la durée moyenne des films par intervalles de 2 ans, ordonné par yearInterval du plus récent au plus vieux
+    val withYear = durations.withColumn("release_year", col("release_year").cast(IntegerType))
+    val withInterval = withYear.withColumn("yearInterval", concat((col("release_year") - (col("release_year") % 2)).cast(StringType), lit("-"), (col("release_year") - (col("release_year") % 2) + 1).cast(StringType)))
+    val avgDurationByInterval = withInterval.groupBy("yearInterval").agg(avg("duration").as("avg_duration"))
+        .withColumn("startYear", regexp_extract(col("yearInterval"), "\\d{4}", 0).cast(IntegerType))
+        .orderBy(desc("startYear"))
+
+    avgDurationByInterval.select("yearInterval", "avg_duration").show()
+
+    // Quel est le duo réalisateur-acteur qui a collaboré dans le plus de films?
+    val explodedCast = cleanedDf.withColumn("cast", explode(split(col("cast"), ", ")))
+    val directorActorPairs = explodedCast.withColumn("director_actor", concat_ws(" - ", col("director"), col("cast")))
+    val mostFrequentPair = directorActorPairs.groupBy("director_actor").count().orderBy(desc("count")).first()
+
+    println(s"Le duo realisateur-acteur qui a collabore dans le plus de films est '${mostFrequentPair.getString(0)}' avec ${mostFrequentPair.getLong(1)} collaborations.")
 }
--- src/main/scala/Twitter.scala
+++ src/main/scala/Twitter.scala
@@ -3,51 +3,51 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 object Twitter {
-    var pathRoute ="../../../inputs/trump_insult_tweets_2014_to_2021.csv"
+    var pathRoute ="inputs/trump_insult_tweets_2014_to_2021.csv"
     val spark = SparkSession.builder.getOrCreate()
 
-    val df = spark.read.option("header", true).option("multiLine", true).option("escape", "\"").csv(pathRoute.split(";").mkString(","))
+    // val df = spark.read.option("header", true).option("multiLine", true).option("escape", "\"").csv(pathRoute.split(";").mkString(","))
+    val df = spark.read.option("header", true).option("inferSchema", "true").option("multiLine", true).option("escape", "\"").csv(pathRoute.split(";").mkString(","))
 
 // Afficher les 7 comptes que Donald Trump insulte le plus (en nombres et en pourcentages)
-    // val insultCounts = df.groupBy("target").count()
-    // val totalInsults = insultCounts.agg(sum("count")).first().getLong(0)
-    // val insultPercentages = insultCounts.withColumn("percentage", (insultCounts("count") / totalInsults) * 100)
-    // val sortedInsultPercentages = insultPercentages.orderBy(-insultPercentages("count"))
-    // sortedInsultPercentages.show(7)
+    val insultCounts = df.groupBy("target").count()
+    val totalInsults = insultCounts.agg(sum("count")).first().getLong(0)
+    val insultPercentages = insultCounts.withColumn("percentage", (insultCounts("count") / totalInsults) * 100)
+    val sortedInsultPercentages = insultPercentages.orderBy(-insultPercentages("count"))
+    sortedInsultPercentages.show(7)
 
 
 // Afficher les insultes que Donald Trump utilise le plus (en nombres et en pourcentages)
-    // val insultCounts2 = df.groupBy("insult").count()
-    // val totalInsults2 = insultCounts2.agg(sum("count")).first().getLong(0)
-    // val insultPercentages2 = insultCounts2.withColumn("percentage", (insultCounts2("count") / totalInsults2) * 100)
-    // val sortedInsultPercentages2 = insultPercentages2.orderBy(-insultPercentages2("count"))
-    // sortedInsultPercentages2.show()
+    val insultCounts2 = df.groupBy("insult").count()
+    val totalInsults2 = insultCounts2.agg(sum("count")).first().getLong(0)
+    val insultPercentages2 = insultCounts2.withColumn("percentage", (insultCounts2("count") / totalInsults2) * 100)
+    val sortedInsultPercentages2 = insultPercentages2.orderBy(-insultPercentages2("count"))
+    sortedInsultPercentages2.show()
 
 // Quelle est l'insulte que Donald Trump utilise le plus pour Joe Biden?
-    // val bidenInsults = df.filter(col("target") === "joe-biden")
-    // val bidenInsultCounts = bidenInsults.groupBy("insult").count()
-    // val mostRepeatedInsult = bidenInsultCounts.orderBy(desc("count")).first()
+    val bidenInsults = df.filter(col("target") === "joe-biden")
+    val bidenInsultCounts = bidenInsults.groupBy("insult").count()
+    val mostRepeatedInsult = bidenInsultCounts.orderBy(desc("count")).first()
 
-    // println(s"L'insulte la plus repetee est '${mostRepeatedInsult.getString(0)}'  ${mostRepeatedInsult.getLong(1)} fois.")
+    println(s"L'insulte la plus repetee est '${mostRepeatedInsult.getString(0)}'  ${mostRepeatedInsult.getLong(1)} fois.")
 
 
 // Combien de fois a-t-il tweeté le mot "Mexico"? Le mot "China"? Le mot "coronavirus"? 
-    // val mexicoTweets = df.filter(lower(col("tweet")).contains("mexico"))
-    // val chinaTweets = df.filter(lower(col("tweet")).contains("china"))
-    // val coronavirusTweets = df.filter(lower(col("tweet")).contains("coronavirus"))
+    val mexicoTweets = df.filter(lower(col("tweet")).contains("mexico"))
+    val chinaTweets = df.filter(lower(col("tweet")).contains("china"))
+    val coronavirusTweets = df.filter(lower(col("tweet")).contains("coronavirus"))
     
-    // println(s"Nombre de tweets contenant 'Mexico': ${mexicoTweets.count()}")
-    // println(s"Nombre de tweets contenant 'China': ${chinaTweets.count()}")
-    // println(s"Nombre de tweets contenant 'coronavirus': ${coronavirusTweets.count()}")
+    println(s"Nombre de tweets contenant 'Mexico': ${mexicoTweets.count()}")
+    println(s"Nombre de tweets contenant 'China': ${chinaTweets.count()}")
+    println(s"Nombre de tweets contenant 'coronavirus': ${coronavirusTweets.count()}")
 
 
 // Classer le nombre de tweets par période de 6 mois
-// val withDate = df.withColumn("date", to_date(col("date"), "yyyy-MM-dd"))
-// val withPeriodStartEnd = withDate.withColumn("periodStartEnd", 
-//     concat(date_format(trunc(col("date"), "yyyy"), "MM-yyyy"), lit(" - "), 
-//     date_format(add_months(trunc(col("date"), "yyyy"), 6), "MM-yyyy")))
-// val tweetAmountByPeriod = withPeriodStartEnd.groupBy("periodStartEnd").count().orderBy("periodStartEnd")
+    val withDate = df.withColumn("date", to_date(col("date"), "yyyy-MM-dd"))
+    val withPeriodStartEnd = withDate.withColumn("periodStartEnd", 
+        concat(date_format(trunc(col("date"), "yyyy"), "MM-yyyy"), lit(" - "), 
+        date_format(add_months(trunc(col("date"), "yyyy"), 6), "MM-yyyy")))
+    val tweetAmountByPeriod = withPeriodStartEnd.groupBy("periodStartEnd").count().orderBy("periodStartEnd")
 
-// tweetAmountByPeriod.show()
-
+    tweetAmountByPeriod.show()
 }
